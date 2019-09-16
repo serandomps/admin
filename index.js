d4aadb796bbf0b79a533d67bb35b1f3b83d766ef
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2019-09-16 11:26:16 +0530

    updating

--- index.js
+++ index.js
@@ -23,14 +23,12 @@ var can = function (permission) {
         if (ctx.token) {
             return next();
         }
-        serand.emit('user', 'login', ctx.path);
+        utils.emit('user', 'login', ctx.path);
     };
 };
 
 page(function (ctx, next) {
-    serand.emit('loader', 'start', {
-        delay: 500
-    });
+    utils.loading();
     next();
 });
 
@@ -38,7 +36,7 @@ page('/signin', auth.signin);
 
 page('/signup', function (ctx, next) {
     var query = ctx.query | {};
-    serand.emit('user', 'login', query.dest || '/');
+    utils.emit('user', 'login', query.dest || '/');
 });
 
 page('/auth', function (ctx, next) {
@@ -51,9 +49,9 @@ page('/auth', function (ctx, next) {
         refresh: sera.refresh
     };
     if (o.username) {
-        return serand.emit('user', 'initialize', o);
+        return utils.emit('user', 'initialize', o);
     }
-    serand.emit('user', 'logged out');
+    utils.emit('user', 'logged out');
 });
 
 page('/', function (ctx, next) {
@@ -66,6 +64,8 @@ page('/', function (ctx, next) {
         .area('#middle')
         .add('admin-client:home')
         .add('vehicles:featured')
+        .area('#footer')
+        .add('footer')
         .render(ctx, next);
 });
 
@@ -76,6 +76,8 @@ page('/vehicles', function (ctx, next) {
         //.add('breadcrumb')
         .area('#middle')
         .add('admin-client:vehicles', {query: ctx.query})
+        .area('#footer')
+        .add('footer')
         .render(ctx, next);
 });
 
@@ -86,6 +88,8 @@ page('/contacts', function (ctx, next) {
         //.add('breadcrumb')
         .area('#middle')
         .add('admin-client:contacts', {query: ctx.query})
+        .area('#footer')
+        .add('footer')
         .render(ctx, next);
 });
 
@@ -95,6 +99,8 @@ page('/create-vehicles', can('vehicle:create'), function (ctx, next) {
         .add('admin-client:navigation')
         .area('#middle')
         .add('vehicles:create')
+        .area('#footer')
+        .add('footer')
         .render(ctx, next);
 });
 
@@ -107,6 +113,8 @@ page('/vehicles/:id', can('vehicle:read'), function (ctx, next) {
         .add('vehicles:findone', {
             id: ctx.params.id
         })
+        .area('#footer')
+        .add('footer')
         .render(ctx, next);
 });
 
@@ -119,6 +127,8 @@ page('/contacts/:id/review', can('vehicle:read'), function (ctx, next) {
         .add('contacts:review', {
             id: ctx.params.id
         })
+        .area('#footer')
+        .add('footer')
         .render(ctx, next);
 });
 
@@ -131,6 +141,8 @@ page('/vehicles/:id/review', can('vehicle:update'), function (ctx, next) {
         .add('vehicles:review', {
             id: ctx.params.id
         })
+        .area('#footer')
+        .add('footer')
         .render(ctx, next);
 });
 
@@ -143,6 +155,8 @@ page('/vehicles/:id/edit', can('vehicle:update'), function (ctx, next) {
         .add('vehicles:create', {
             id: ctx.params.id
         })
+        .area('#footer')
+        .add('footer')
         .render(ctx, next);
 });
 
@@ -155,6 +169,8 @@ page('/contacts/:id/edit', can('vehicle:update'), function (ctx, next) {
         .add('contacts:create', {
             id: ctx.params.id
         })
+        .area('#footer')
+        .add('footer')
         .render(ctx, next);
 });
 
@@ -167,6 +183,8 @@ page('/vehicles/:id/delete', can('vehicle:update'), function (ctx, next) {
         .add('vehicles:remove', {
             id: ctx.params.id
         })
+        .area('#footer')
+        .add('footer')
         .render(ctx, next);
 });
 
@@ -176,12 +194,14 @@ page('/mine', can('user'), function (ctx, next) {
         .add('admin-client:navigation')
         .area('#middle')
         .add('vehicles:mine')
+        .area('#footer')
+        .add('footer')
         .render(ctx, next);
 });
 
 //TODO: redirect user to login page when authentication is needed
 //TODO: basically a front controller pattern
-serand.on('user', 'login', function (path) {
+utils.on('user', 'login', function (path) {
     var ctx;
     if (!path) {
         ctx = serand.current();
@@ -190,7 +210,7 @@ serand.on('user', 'login', function (path) {
     serand.store('state', {
         path: path
     });
-    serand.emit('user', 'authenticator', {
+    utils.emit('user', 'authenticator', {
         type: 'serandives',
         location: loginUri
     }, function (err, uri) {
@@ -198,14 +218,14 @@ serand.on('user', 'login', function (path) {
     });
 });
 
-serand.on('user', 'logged in', function (usr) {
+utils.on('user', 'logged in', function (usr) {
     var state = serand.store('state', null);
     redirect(state && state.path || '/');
 });
 
-serand.on('user', 'logged out', function (usr) {
+utils.on('user', 'logged out', function (usr) {
     var state = serand.store('state', null);
     redirect(state && state.path || '/');
 });
 
-serand.emit('serand', 'ready');
+utils.emit('serand', 'ready');
